1ee2f436374230d0e0c0b6c17646acb2bb0c9a7e
Sahil Singh <sahilsingh00160803@gmail.com>
2024-10-20 11:17:04 +0530

    Define routes for rule creation and evaluation

--- routes/ruleRoutes.js
+++ routes/ruleRoutes.js
@@ -2,7 +2,20 @@ import express from "express";
 import { createRule, evaluateRule } from "../controllers/ruleController.js";
 const router = express.Router();
 
-router.post("/create", createRule);
-router.post("/evaluate", evaluateRule);
+router.post("/create", (req, res, next) => {
+    console.log("Route hit: /create");
+    next();
+}, createRule);
+
+router.post("/evaluate", (req, res, next) => {
+    console.log("Route hit: /evaluate");
+    next();
+}, evaluateRule);
+
+router.get("/test", (req, res) => {
+    console.log("Route hit: /test");
+    res.status(200).send("Test route is working");
+});
+
 
 export default router;
